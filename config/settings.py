3ea774da4cd44dbd0e55156c1889f4dcc102669f
SIZNING_GITHUB_USERNAME <SIZNING_EMAIL>
2025-09-25 09:36:58 +0500

    test

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'db',   # '127.0.0.1'
+        'HOST': os.environ.get('DB_HOST', 'localhost'),   # '127.0.0.1'
         'PORT': '5432',
     }
 }
